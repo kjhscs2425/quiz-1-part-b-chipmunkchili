14f9226dbcf1a2fb999d2933c95e6f7173f4fbe4
Stella Talcott <stalcott2027@kehillahstudent.org>
2024-10-23 10:42:09 -0700

    completed drawing

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import turtle
+from turtle import *
 
-# YOUR CODE HERE
+#This for loop draws the outer square, with side lengths of 100 pixels
+for i in range(4):
+    forward(100)
+    right(90)
 
-turtle.exitonclick()
+# This section repositions the turtle to draw the next square
+forward(50)
+right(45)
+
+# This for loop draws the inner square, with side lengths of 71 pixels
+for i in range (4):
+    forward(71)
+    right(90)
+
+exitonclick()
